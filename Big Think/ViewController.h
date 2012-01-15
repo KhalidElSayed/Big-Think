d544293e5d9f900bdf7873412d59f8707d1ab213
Richard Kirk <RichardBKirk@gmail.com>
2012-01-15 15:25:37 -0600

    Initial Commit

---
+++ Big Think/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Big Think
+//
+//  Created by Richard Kirk on 1/15/12.
+//  Copyright (c) 2012 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
